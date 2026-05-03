99654c7061abdc59f70360e152984c3b8d37d111
Rajesh Aravind <rajesharavind@Rajeshs-MacBook-Pro-2.local>
2026-05-03 16:47:21 +0530

    fix production API URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const api = axios.create({ 
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001' 
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001'
 });
 export default api;
